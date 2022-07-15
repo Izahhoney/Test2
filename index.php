bab0f921f4671bfb990ff0bd74579681a1e30a3c
Izahhoney <2020489338@student.uitm.edu.my>
2022-07-15 21:25:57 +0800

    sddfdfdfe

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <title>Run Event</title>
+
+
+</head>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/62d0ed287b967b1179999e42/1g801sptl';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+<body>
+<img src="chocolate honey yougurt.png" alt="honey" style="width:70%">
+   <h1> Fun Run </h1>
+   <p>Date: 7 Mei 2020</p>
+   <p>Distance: 15KM</p>
+   <p>Date: Rainbow</p>
+
+   <body>
+
+   <div class="w3-container w3-pale-red">
+  <h1>My Car</h1>
+</div>
+
+
+<div class="w3-container">
+  <p>Let's stay fit and Healty</p>
+</div>
+   <p class="login-register-text">Do you interesting about that? <a href="register.php">Register Here</a>.</p>
+</body>
+</html>
